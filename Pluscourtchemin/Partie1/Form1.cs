6a5739510f6422b3157c9a03e9c6e3e705b58868
Marie Gibert <mgibert001@ensc.fr>
2018-10-16 11:21:52 +0200

    Fichier xml de test

--- Pluscourtchemin/Partie1/Form1.cs
+++ Pluscourtchemin/Partie1/Form1.cs
@@ -3,24 +3,40 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace Partie1
 {
     public partial class Questionnaire : Form
     {
+        List<Question> questions;
+
         public Questionnaire()
         {
             InitializeQuestion();
             InitializeComponent();
+            questions = new List<Question>();
         }
 
         private void InitializeQuestion()
         {
             throw new NotImplementedException();
         }
+
+        public void RecupToutesQuestions()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Question));
+            using (StreamReader rd = new StreamReader("QuestionsReponses.xml"))
+            {
+                Question q = xs.Deserialize(rd) as Question;
+                questions.Add(q);
+            }
+        }
+        
     }
 }
