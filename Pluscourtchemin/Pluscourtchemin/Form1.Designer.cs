1d49f62288efbc396d70ca204712281d7d749d45
mgibert001 <mgibert001@TP329>
2018-11-13 14:43:28 +0100

    Historique utilisateur et interface Form1

--- Pluscourtchemin/Pluscourtchemin/Form1.Designer.cs
+++ Pluscourtchemin/Pluscourtchemin/Form1.Designer.cs
@@ -39,6 +39,13 @@
             this.buttonInit2 = new System.Windows.Forms.Button();
             this.listBoxgraphe = new System.Windows.Forms.ListBox();
             this.textTitre = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.champOuverts = new System.Windows.Forms.TextBox();
+            this.champFermes = new System.Windows.Forms.TextBox();
+            this.txtInstruction = new System.Windows.Forms.Label();
+            this.txtOuvert = new System.Windows.Forms.Label();
+            this.txtFerme = new System.Windows.Forms.Label();
+            this.btn_Valider = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonInit1
@@ -53,7 +60,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(171, 78);
+            this.textBox1.Location = new System.Drawing.Point(171, 97);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -61,7 +68,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(311, 78);
+            this.textBox2.Location = new System.Drawing.Point(311, 97);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
@@ -69,7 +76,7 @@
             // 
             // buttonAEtoile
             // 
-            this.buttonAEtoile.Location = new System.Drawing.Point(479, 22);
+            this.buttonAEtoile.Location = new System.Drawing.Point(584, 76);
             this.buttonAEtoile.Name = "buttonAEtoile";
             this.buttonAEtoile.Size = new System.Drawing.Size(75, 23);
             this.buttonAEtoile.TabIndex = 3;
@@ -80,7 +87,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(168, 54);
+            this.label1.Location = new System.Drawing.Point(168, 73);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 4;
@@ -89,7 +96,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(308, 54);
+            this.label2.Location = new System.Drawing.Point(308, 73);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 5;
@@ -98,21 +105,21 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(336, 141);
+            this.listBox1.Location = new System.Drawing.Point(171, 210);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(128, 121);
             this.listBox1.TabIndex = 6;
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(479, 117);
+            this.treeView1.Location = new System.Drawing.Point(550, 114);
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(152, 180);
             this.treeView1.TabIndex = 7;
             // 
             // buttonInit2
             // 
-            this.buttonInit2.Location = new System.Drawing.Point(35, 67);
+            this.buttonInit2.Location = new System.Drawing.Point(47, 95);
             this.buttonInit2.Name = "buttonInit2";
             this.buttonInit2.Size = new System.Drawing.Size(75, 23);
             this.buttonInit2.TabIndex = 8;
@@ -123,8 +130,8 @@
             // listBoxgraphe
             // 
             this.listBoxgraphe.FormattingEnabled = true;
-            this.listBoxgraphe.Location = new System.Drawing.Point(171, 133);
-            this.listBoxgraphe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBoxgraphe.Location = new System.Drawing.Point(35, 148);
+            this.listBoxgraphe.Margin = new System.Windows.Forms.Padding(2);
             this.listBoxgraphe.Name = "listBoxgraphe";
             this.listBoxgraphe.Size = new System.Drawing.Size(100, 121);
             this.listBoxgraphe.TabIndex = 9;
@@ -139,11 +146,77 @@
             this.textTitre.TabIndex = 10;
             this.textTitre.Text = "Test Dijkstra";
             // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(356, 210);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(128, 121);
+            this.listBox2.TabIndex = 11;
+            // 
+            // champOuverts
+            // 
+            this.champOuverts.Location = new System.Drawing.Point(314, 159);
+            this.champOuverts.Name = "champOuverts";
+            this.champOuverts.Size = new System.Drawing.Size(100, 20);
+            this.champOuverts.TabIndex = 12;
+            // 
+            // champFermes
+            // 
+            this.champFermes.Location = new System.Drawing.Point(169, 159);
+            this.champFermes.Name = "champFermes";
+            this.champFermes.Size = new System.Drawing.Size(100, 20);
+            this.champFermes.TabIndex = 13;
+            // 
+            // txtInstruction
+            // 
+            this.txtInstruction.AutoSize = true;
+            this.txtInstruction.Location = new System.Drawing.Point(171, 43);
+            this.txtInstruction.Name = "txtInstruction";
+            this.txtInstruction.Size = new System.Drawing.Size(113, 13);
+            this.txtInstruction.TabIndex = 14;
+            this.txtInstruction.Text = "Ecrire ici la consigne...";
+            // 
+            // txtOuvert
+            // 
+            this.txtOuvert.AutoSize = true;
+            this.txtOuvert.Location = new System.Drawing.Point(314, 140);
+            this.txtOuvert.Name = "txtOuvert";
+            this.txtOuvert.Size = new System.Drawing.Size(53, 13);
+            this.txtOuvert.TabIndex = 15;
+            this.txtOuvert.Text = "Ouverts : ";
+            // 
+            // txtFerme
+            // 
+            this.txtFerme.AutoSize = true;
+            this.txtFerme.Location = new System.Drawing.Point(172, 140);
+            this.txtFerme.Name = "txtFerme";
+            this.txtFerme.Size = new System.Drawing.Size(47, 13);
+            this.txtFerme.TabIndex = 16;
+            this.txtFerme.Text = "Fermés :";
+            // 
+            // btn_Valider
+            // 
+            this.btn_Valider.Location = new System.Drawing.Point(426, 159);
+            this.btn_Valider.Name = "btn_Valider";
+            this.btn_Valider.Size = new System.Drawing.Size(109, 23);
+            this.btn_Valider.TabIndex = 17;
+            this.btn_Valider.Text = "Valider cette étape";
+            this.btn_Valider.UseVisualStyleBackColor = true;
+            this.btn_Valider.Click += new System.EventHandler(this.btn_Valider_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 350);
+            this.Controls.Add(this.btn_Valider);
+            this.Controls.Add(this.txtFerme);
+            this.Controls.Add(this.txtOuvert);
+            this.Controls.Add(this.txtInstruction);
+            this.Controls.Add(this.champFermes);
+            this.Controls.Add(this.champOuverts);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.textTitre);
             this.Controls.Add(this.listBoxgraphe);
             this.Controls.Add(this.buttonInit2);
@@ -157,6 +230,7 @@
             this.Controls.Add(this.buttonInit1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,6 +249,13 @@
         private System.Windows.Forms.Button buttonInit2;
         private System.Windows.Forms.ListBox listBoxgraphe;
         private System.Windows.Forms.Label textTitre;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox champOuverts;
+        private System.Windows.Forms.TextBox champFermes;
+        private System.Windows.Forms.Label txtInstruction;
+        private System.Windows.Forms.Label txtOuvert;
+        private System.Windows.Forms.Label txtFerme;
+        private System.Windows.Forms.Button btn_Valider;
     }
 }
 
--- Pluscourtchemin/Pluscourtchemin/Form1.cs
+++ Pluscourtchemin/Pluscourtchemin/Form1.cs
@@ -18,28 +18,36 @@ namespace Pluscourtchemin
         static public int nbnodes = 10;
         static public int numinitial;
         static public int numfinal;
+
+        public List<List<GenericNode>> historiqueUtiOuvert;
+        public List<List<GenericNode>> historiqueUtiFerme;
+
         //ajout d'un commentair einutile pour tester git
         // test micro
         public Form1()
         {
             InitializeComponent();
         }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
 
         private void buttonInit1_Click(object sender, EventArgs e)
         {
-           
+
             matrice = new double[nbnodes, nbnodes];
             for (int i = 0; i < nbnodes; i++)
                 for (int j = 0; j < nbnodes; j++)
                     matrice[i, j] = -1;
 
-            matrice[0, 1] = 3;   matrice[1, 0] = 3;
-            matrice[0, 2] = 5;      matrice[2, 0] = 5;
-            matrice[0, 3] = 7;      matrice[3, 0] = 7;
-            matrice[1, 4] = 8;      matrice[4, 1] = 8;
-            matrice[2, 4] = 3;      matrice[4, 2] = 3;
-            matrice[4, 5] = 7;      matrice[5, 4] = 7;
-            matrice[5, 6] = 4;      matrice[6, 5] = 4;
+            matrice[0, 1] = 3; matrice[1, 0] = 3;
+            matrice[0, 2] = 5; matrice[2, 0] = 5;
+            matrice[0, 3] = 7; matrice[3, 0] = 7;
+            matrice[1, 4] = 8; matrice[4, 1] = 8;
+            matrice[2, 4] = 3; matrice[4, 2] = 3;
+            matrice[4, 5] = 7; matrice[5, 4] = 7;
+            matrice[5, 6] = 4; matrice[6, 5] = 4;
         }
 
         private void buttonAEtoile_Click(object sender, EventArgs e)
@@ -52,20 +60,25 @@ namespace Pluscourtchemin
             List<GenericNode> solution = g.RechercheSolutionAEtoile(N0);
 
             Node2 N1 = N0;
-            for (int i=1; i < solution.Count; i++)
+            for (int i = 1; i < solution.Count; i++)
             {
                 Node2 N2 = (Node2)solution[i];
-                listBox1.Items.Add(Convert.ToString(N1.numero) 
-                     + "--->"  + Convert.ToString(N2.numero)
-                     + "   : " +Convert.ToString(matrice[N1.numero,N2.numero]));
+                listBox1.Items.Add(Convert.ToString(N1.numero)
+                     + "--->" + Convert.ToString(N2.numero)
+                     + "   : " + Convert.ToString(matrice[N1.numero, N2.numero]));
                 N1 = N2;
             }
 
             g.GetSearchTree(treeView1);
+
+            // lancer la méthode de correction
         }
 
         private void buttonInit2_Click(object sender, EventArgs e)
         {
+            //Initialiser les historiques pour ce graphe
+            historiqueUtiFerme = new List<List<GenericNode>>();
+            historiqueUtiOuvert = new List<List<GenericNode>>();
 
             StreamReader monStreamReader = new StreamReader("graphe1.txt");
 
@@ -77,8 +90,10 @@ namespace Pluscourtchemin
             string strnbnoeuds = "";
             i++; // On dépasse le ":"
             while (ligne[i] == ' ') i++; // on saute les blancs éventuels
-            while (i < ligne.Length) { strnbnoeuds = strnbnoeuds + ligne[i];
-                i++; 
+            while (i < ligne.Length)
+            {
+                strnbnoeuds = strnbnoeuds + ligne[i];
+                i++;
             }
             nbnodes = Convert.ToInt32(strnbnoeuds);
 
@@ -118,7 +133,7 @@ namespace Pluscourtchemin
                 // On saute les blancs éventuels
                 while (ligne[i] == ' ') i++;
                 string strVal = "";
-                while ((i < ligne.Length) && (ligne[i] !=' '))
+                while ((i < ligne.Length) && (ligne[i] != ' '))
                 {
                     strVal = strVal + ligne[i];
                     i++;
@@ -129,7 +144,7 @@ namespace Pluscourtchemin
                 matrice[N2, N1] = val;
                 listBoxgraphe.Items.Add(Convert.ToString(N1)
                    + "--->" + Convert.ToString(N2)
-                   + "   : " + Convert.ToString(matrice[N1, N2]));            
+                   + "   : " + Convert.ToString(matrice[N1, N2]));
 
                 ligne = monStreamReader.ReadLine();
             }
@@ -139,5 +154,60 @@ namespace Pluscourtchemin
 
 
         }
+
+        private void btn_Valider_Click(object sender, EventArgs e)
+        {
+            string ouvert = txtOuvert.Text;
+            string ferme = txtFerme.Text;
+            if ((ouvert == "") && (ferme != ""))
+            {
+                //faire A* et correction (faire une méthode pour correction) (et la lancer depuis a*)
+            }
+            else
+            {
+                List<GenericNode> listeOuvert = new List<GenericNode>();
+                List<GenericNode> listeFerme = new List<GenericNode>();
+
+                //récupérer champOuvert et champFermes
+
+
+                foreach (char c in ouvert)
+                {
+                    if (c != ',')
+                    {
+                        try
+                        {
+                            int x = (int)c;
+                            Node2 N = new Node2();
+                            N.numero = x;
+                            listeOuvert.Add(N);
+                        }
+                        catch { }
+                    }
+                }
+
+                foreach (char c in ferme)
+                {
+                    if (c != ',')
+                    {
+                        try
+                        {
+                            int x = (int)c;
+                            Node2 N = new Node2();
+                            N.numero = x;
+                            listeFerme.Add(N);
+                        }
+                        catch { }
+                    }
+                }
+
+                //le mettre dans une liste statique.
+
+                historiqueUtiFerme.Add(listeFerme);
+                historiqueUtiOuvert.Add(listeOuvert);
+            }
+        }
+
     }
+
 }
