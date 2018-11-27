2b1afac187116203748459eb5886b038f4a65bbd
Marie150 <mgibert001@ensc.fr>
2018-11-27 17:20:56 +0100

    Réajustement taille des forms

--- Partie1/Accueil.Designer.cs
+++ Partie1/Accueil.Designer.cs
@@ -38,11 +38,10 @@
             // btnCommencer
             // 
             this.btnCommencer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCommencer.Location = new System.Drawing.Point(410, 206);
-            this.btnCommencer.Location = new System.Drawing.Point(242, 167);
-            this.btnCommencer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCommencer.Location = new System.Drawing.Point(427, 206);
+            this.btnCommencer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCommencer.Name = "btnCommencer";
-            this.btnCommencer.Size = new System.Drawing.Size(155, 35);
+            this.btnCommencer.Size = new System.Drawing.Size(207, 43);
             this.btnCommencer.TabIndex = 0;
             this.btnCommencer.Text = "Commencer le test";
             this.btnCommencer.UseVisualStyleBackColor = true;
@@ -52,22 +51,19 @@
             // 
             this.text1.AutoSize = true;
             this.text1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text1.Location = new System.Drawing.Point(349, 30);
-            this.text1.Location = new System.Drawing.Point(196, 24);
-            this.text1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text1.Location = new System.Drawing.Point(365, 30);
             this.text1.Name = "text1";
-            this.text1.Size = new System.Drawing.Size(252, 26);
+            this.text1.Size = new System.Drawing.Size(328, 32);
             this.text1.TabIndex = 1;
             this.text1.Text = "Questionnaire module IA";
             // 
             // btnQuitter
             // 
             this.btnQuitter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnQuitter.Location = new System.Drawing.Point(410, 272);
-            this.btnQuitter.Location = new System.Drawing.Point(242, 221);
-            this.btnQuitter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnQuitter.Location = new System.Drawing.Point(427, 272);
+            this.btnQuitter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnQuitter.Name = "btnQuitter";
-            this.btnQuitter.Size = new System.Drawing.Size(155, 31);
+            this.btnQuitter.Size = new System.Drawing.Size(207, 38);
             this.btnQuitter.TabIndex = 2;
             this.btnQuitter.Text = "Quitter";
             this.btnQuitter.UseVisualStyleBackColor = true;
@@ -77,38 +73,33 @@
             // 
             this.text2.AutoSize = true;
             this.text2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.text2.Location = new System.Drawing.Point(282, 111);
-            this.text2.Location = new System.Drawing.Point(146, 90);
-            this.text2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text2.Location = new System.Drawing.Point(299, 111);
             this.text2.Name = "text2";
-            this.text2.Size = new System.Drawing.Size(379, 24);
+            this.text2.Size = new System.Drawing.Size(481, 29);
             this.text2.TabIndex = 3;
             this.text2.Text = "Testez vos connaissances en 20 questions !";
             // 
             // texteAuteur
             // 
             this.texteAuteur.AutoSize = true;
-            this.texteAuteur.Location = new System.Drawing.Point(605, 375);
-            this.texteAuteur.Location = new System.Drawing.Point(388, 305);
-            this.texteAuteur.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.texteAuteur.Location = new System.Drawing.Point(621, 375);
             this.texteAuteur.Name = "texteAuteur";
-            this.texteAuteur.Size = new System.Drawing.Size(277, 13);
+            this.texteAuteur.Size = new System.Drawing.Size(371, 17);
             this.texteAuteur.TabIndex = 4;
             this.texteAuteur.Text = "Projet IA - Ghennam Younes, Gibert Marie, Mehamli Théo";
             // 
             // Accueil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.texteAuteur);
             this.Controls.Add(this.text2);
             this.Controls.Add(this.btnQuitter);
             this.Controls.Add(this.text1);
             this.Controls.Add(this.btnCommencer);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Accueil";
             this.Size = new System.Drawing.Size(1080, 427);
-            this.Size = new System.Drawing.Size(685, 334);
             this.ResumeLayout(false);
             this.PerformLayout();
 
